4d55fac89e75b399d7e9b4b48503452a28c0775c
Silas Mokone <silas.mokone@vodacom.co.za>
2022-03-17 14:47:07 +0200

    redux login and register logic complete

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -7,7 +7,23 @@ const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
 
   if (response.data) {
-    localStorage.SetItem("user", JSON.stringify(response.data));
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+//logout
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+//Login a user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
 
   return response.data;
@@ -15,6 +31,8 @@ const register = async (userData) => {
 
 const authService = {
   register,
+  logout,
+  login
 };
 
 export default authService;
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -30,6 +30,35 @@ export const register = createAsyncThunk(
   }
 );
 
+
+//Logout
+export const logout = createAsyncThunk('auth/logout',async () => {
+
+  await authService.logout()
+
+});
+
+
+//Login user
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.login(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -56,7 +85,24 @@ export const authSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.user = null;
-      });
+      })
+     .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled,(state) => {
+        state.user = null
+      })
   },
 });
 
